420a83344b5e735ac39ae5b043c763b32750c23c
Shruti Agarwal <105115758+ShrutiAgarwal1988@users.noreply.github.com>
2022-10-23 20:10:17 +0530

    dummy project
    project structure

---
+++ spring-boot-Employee-Management-Design/src/main/java/com/greatlearning/springbootEmployeeManagementDesign/SpringBootEmployeeManagementDesignApplication.java
@@ -0,0 +1,13 @@
+package com.greatlearning.springbootEmployeeManagementDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeManagementDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeManagementDesignApplication.class, args);
+	}
+
+}
